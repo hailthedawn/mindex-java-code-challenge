d1725597354a2fee941ab790aa6fecf0948fa33c
hailthedawn <kkt7778@rit.edu>
2023-03-26 17:05:18 +0530

    Compensation Model added

--- src/main/java/com/mindex/challenge/data/Compensation.java
+++ src/main/java/com/mindex/challenge/data/Compensation.java
@@ -1,2 +1,45 @@
-package com.mindex.challenge.data;public class Compensation {
+package com.mindex.challenge.data;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+
+import java.time.LocalDate;
+
+
+public class Compensation {
+
+    private String compensationId;
+    @DBRef
+    private Employee employee;
+    @JsonFormat(pattern="MM/dd/yyyy")
+    private LocalDate effectiveDate;
+    private double salary;
+
+    public void setCompensationId(String compensationId) {
+        this.compensationId = compensationId;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public void setEffectiveDate(LocalDate effectiveDate) {
+        this.effectiveDate = effectiveDate;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public LocalDate getEffectiveDate() {
+        return effectiveDate;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
 }
